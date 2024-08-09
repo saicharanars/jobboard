927edb6655f91005556bb47bc754246d85db4447
saicharanars <saicharan_@outlook.com>
2024-08-09 11:36:59 +0530

    Fix bug related to 'uopdatedto' field

--- jobboard-monorepo/apps/api/src/applications/dto/update-application.dto.ts
+++ jobboard-monorepo/apps/api/src/applications/dto/update-application.dto.ts
@@ -1,18 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUrl, IsArray, IsString, IsEnum } from 'class-validator';
+import { IsUrl, IsArray, IsString, IsEnum, IsOptional } from 'class-validator';
 import { statusEnum } from '../entities/application.entity';
 
 export class UpdateApplicationDto {
+  @IsOptional()
   @IsUrl()
   @ApiProperty({
     description: 'resume url',
   })
   resume_url?: string;
+  @IsOptional()
   @IsEnum(statusEnum)
   @ApiProperty({
     description: 'status of application',
   })
   status?: statusEnum;
+  @IsOptional()
   @IsArray()
   @IsString({ each: true })
   @ApiProperty({
